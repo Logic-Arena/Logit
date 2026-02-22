3b1063de45619cba3d89e17bff2c5122c1d31bf0
gunrein <gunrein620@gmail.com>
2026-02-23 01:28:53 +0900

    feat:배포환경 설정

--- logic-arena-frontend/src/components/layout/Header.tsx
+++ logic-arena-frontend/src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export function Header() {
   return (
     <header className="app-header">
-      <Link to="/" className="app-header__logo">Logic Arena</Link>
+      <Link to="/" className="app-header__logo">Logit</Link>
     </header>
   );
 }
